a0bc2fccaec43ffa83a1a7c5b6adc469458d1137
Mariano Losada <89931647+marianodip12@users.noreply.github.com>
2026-03-08 21:20:27 -0300

    Delete handball-pro-v6 (1) directory

--- handball-pro-v6 (1)/src/main.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { MatchProvider } from './context/MatchContext.jsx'
-import App from './App.jsx'
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode><MatchProvider><App/></MatchProvider></React.StrictMode>
-)
